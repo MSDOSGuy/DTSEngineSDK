353aef2167b5dcc46867c3d3c018914884166f62
MSDOSGuy <64180698+MSDOSGuy@users.noreply.github.com>
2020-08-25 01:40:01 -0400

    Add files via upload

---
+++ DTSEngine/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenTK;
+
+namespace DTCEngine
+{
+    public class RendererFile_0 : GameWindow
+    {
+        Form1.merger m = new Form1.merger(); 
+        public virtual void UpdateOnceFrame()
+        {
+            // engine specific code
+        }
+        public virtual void RenderScene()
+        {
+            // engine specific code
+
+            m.placeholdermerger_0(); // contains scene object, normals, lighting, texture matrices etc...
+        }
+        public virtual void OnLoad() 
+        {
+
+        }
+    }
+}
